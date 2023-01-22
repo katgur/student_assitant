9f4a466265e74b6eaa5fc1f985a7def402eae113
kiiitG <lucky.kitty@mail.ru>
2023-01-22 15:11:44 +0300

    - Фрагменты - Навигация

---
+++ app/src/main/java/com/example/student_assistant/FiltersFragment.kt
@@ -0,0 +1,38 @@
+package com.example.student_assistant
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.student_assistant.databinding.FragmentFiltersBinding
+import com.example.student_assistant.databinding.FragmentMainBinding
+
+class FiltersFragment : Fragment() {
+
+    private lateinit var binding : FragmentFiltersBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFiltersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.filtersIvStatusArrow.setOnClickListener {
+            val action = FiltersFragmentDirections.actionFiltersFragmentToParametersFragment()
+            findNavController().navigate(action)
+        }
+
+        binding.filtersIvTagsArrow.setOnClickListener {
+            val action = FiltersFragmentDirections.actionFiltersFragmentToParametersFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
